e49260897c97fd373c1889415d6c803f3a02ac34
Christophe VG <contact@christophe.vg>
2017-04-04 17:07:39 +0200

    minimaal test programma toegevoegd: blink

---
+++ src/blink/README.md
@@ -0,0 +1,17 @@
+# Blink
+
+Dit is de eenvoudigste test om te controleren dat alles werkt. Het laat de ingebouwde blauwe LED van de Photon blinken.
+
+## Hoe doe je dit werken op de Photon?
+
+Vanuit de `src/` folder, geef je het commando `make`:
+
+```bash
+$ make
+*** Compileren en flashen van blink
+particle flash 370026000447333436363331 blink
+Including:
+    /Users/xtof/Workspace/knutselbaar/a-bot/src/blink/blink.ino
+attempting to flash firmware to your device 370026000447333436363331
+Flash device OK:  Update started
+```
---
+++ src/blink/blink.ino
@@ -0,0 +1,17 @@
+// de eenvoudigste test om te controleren dat alles werkt
+// dit laat de ingebouwde blauwe LED van de Photon blinken
+// author: Christophe VG <contact@christophe.vg>
+
+#define LED    D7      // dit is de standaard led die op pin D7 verbonden is
+
+void setup() {
+  // configureer de LED pin voor uitvoer
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);   // zet LED pin hoog
+  delay(1000);               // wacht voor 1000ms = 1 sec
+  digitalWrite(LED, LOW);    // zet LED pin laag
+  delay(1000);               // en wacht opnieuw 1 sec
+}
