bceafc604d0cb1e746588098a0fc9725cc0823dc
Christophe VG <contact@christophe.vg>
2017-04-06 11:19:53 +0200

    voorbeeld voor aansturing van een motor toegevoegd

---
+++ src/motor/motor.ino
@@ -0,0 +1,58 @@
+// deze app laat een motor draaien
+// author: Christophe VG <contact@christophe.vg>
+
+#define MOTOR1_EN    D0      // dit controleert of de motor in gebruik is
+#define MOTOR1_1A    A0      // dit controleert de draairichting
+#define MOTOR1_2A    A1      // dit controleert de draairichting
+
+#define MIN_SNELHEID   120      // dit is de minimale snelheid
+#define MAX_SNELHEID   255      // dit is de maximale snelheid
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(MOTOR1_EN, OUTPUT);
+  pinMode(MOTOR1_1A, OUTPUT);
+  pinMode(MOTOR1_2A, OUTPUT);
+}
+
+#define SNELHEID 90
+
+void motor_update(int16_t snelheid) {
+  Serial.println(snelheid);
+  // houd snelheid binnen grenzen
+  if(abs(snelheid) < MIN_SNELHEID) { snelheid = 0; }
+  if(abs(snelheid) > MAX_SNELHEID) {
+    snelheid = MAX_SNELHEID * (snelheid/abs(snelheid));
+  }
+
+  // bepaal draairichting
+  if(snelheid == 0) {
+    // stop
+    digitalWrite(MOTOR1_1A, LOW);
+    digitalWrite(MOTOR1_2A, LOW);
+  } else if(snelheid < 0) {
+    // achterwaarts
+    digitalWrite(MOTOR1_1A, HIGH);
+    digitalWrite(MOTOR1_2A, LOW);
+  } else {
+    // voorwaarts
+    digitalWrite(MOTOR1_1A, LOW);
+    digitalWrite(MOTOR1_2A, HIGH);
+  }
+  // PWM
+  analogWrite(MOTOR1_EN, abs(snelheid));
+}
+
+void loop() {
+  // voorwaarts
+  for(int16_t snelheid=60; snelheid<300; snelheid+=20) {
+    motor_update(snelheid);
+    delay(1000);
+  }
+
+  // achterwaarts
+  for(int16_t snelheid=-60; snelheid>-300; snelheid-=20) {
+    motor_update(snelheid);
+    delay(1000);
+  }
+}
